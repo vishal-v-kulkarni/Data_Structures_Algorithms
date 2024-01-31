a2b20e9aa75c42a540f474d545cc349dc6fd9ae9
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-01-31 14:02:00 -0500

    Array as Parameter

--- 7_Parameter_Passing_Array_Struct/array_as_parameter.cpp
+++ 7_Parameter_Passing_Array_Struct/array_as_parameter.cpp
@@ -1,53 +1,74 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
 
-void fun(int A[],int n){ //We can use *A instead of A[]
-    
-    //cout<<"Size of Array in fun: "<<sizeof(A)/sizeof(int)<<endl; //Size of pointer/size of int as Array is passed as address/pointer
+void fun(int A[], int n)
+{ // We can use *A instead of A[]
 
-    //Cannot use for each loop on a pointer
-    // for(int a:A){
-    //     cout<<a<<endl;
-    // }
+    // cout<<"Size of Array in fun: "<<sizeof(A)/sizeof(int)<<endl; //Size of pointer/size of int as Array is passed as address/pointer
+
+    // Cannot use for each loop on a pointer
+    //  for(int a:A){
+    //      cout<<a<<endl;
+    //  }
 
     // for(int i=0;i<5;i++){
     //     cout<<A[i]<<endl;
     // }
 
     A[0] = 15;
-
 }
 
-int * fun1(int size){ //* for returning array in new compilers
+int *fun1(int size) //* for returning array in new compilers
+{
     int *p;
-    p = new int[size]; //Array created in the HEAP
+    p = new int[size]; // Array created in the HEAP
 
-    for(int i=0;i<size;i++){
-        p[i] = i+1;
+    for (int i = 0; i < size; i++)
+    {
+        p[i] = i + 1;
     }
 
     return p;
 }
 
-int main(){
+// Returning array in C
+int *fun2(int n)
+{
+    int *A;
+    A = (int *)malloc(sizeof(int) * n);
 
-    int A[]={2,4,6,8,10};
-    int n=5;
-    fun(A,n);
-    cout<<"Size of Array in main: "<<sizeof(A)/sizeof(int)<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = i + 1;
+    }
 
-    for(int x:A)
-        cout<<x<<endl;
+    return A;
+}
+
+int main()
+{
+
+    int A[] = {2, 4, 6, 8, 10};
+    int n = 5;
+    fun(A, n);
+    cout << "Size of Array in main: " << sizeof(A) / sizeof(int) << endl;
+
+    for (int x : A)
+        cout << x << endl;
 
     // Function returning array logic
-    int *ptr, size=5;
+    int *ptr, size = 5;
 
     ptr = fun1(size);
 
-    for(int i=0; i<size; i++)
-        cout<<ptr[i]<<endl;
+    for (int i = 0; i < size; i++)
+        cout << ptr[i] << endl;
+
+    // Function returning array in C
+    int B[size];
+    int *C = &B[0];
+    C = fun2(size);
 
     return 0;
-
 }
