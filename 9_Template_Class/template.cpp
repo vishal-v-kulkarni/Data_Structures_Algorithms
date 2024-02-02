bf182c660469e258fc544b5a9de0e79aa809ca27
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-02-01 21:57:17 -0500

    Template Class and Added Debugger

--- 9_Template_Class/template.cpp
+++ 9_Template_Class/template.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-template<class T>
-class Arithmetic{
+template <class T>
+class Arithmetic
+{
     private:
         T num1;
         T num2;
@@ -13,33 +14,37 @@ class Arithmetic{
         T sub();
 };
 
-template<class T>
-Arithmetic<T>::Arithmetic(T num1, T num2){
-    this->num1 = num1;
+template <class T>
+Arithmetic<T>::Arithmetic(T num1, T num2)
+{
+    this->num1 = num1; // this is a pointer, so we need -> to access the members.
     this->num2 = num2;
 }
 
-template<class T>
-T Arithmetic<T>::add(){
-    return num1+num2;
+template <class T>
+T Arithmetic<T>::add()
+{
+    return num1 + num2;
 }
 
-template<class T>
-T Arithmetic<T>::sub(){
-    return num1-num2;
+template <class T>
+T Arithmetic<T>::sub()
+{
+    return num1 - num2;
 }
 
-int main(){
+int main()
+{
 
-    Arithmetic<int> ar1(10,5);
+    Arithmetic<int> ar1(10, 5);
 
-    cout<<"Add of ar1: "<<ar1.add()<<endl;
-    cout<<"Sub of ar1: "<<ar1.sub()<<endl;
+    cout << "Add of ar1: " << ar1.add() << endl;
+    cout << "Sub of ar1: " << ar1.sub() << endl;
 
-    Arithmetic<float> ar2(10.56,5.52);
+    Arithmetic<float> ar2(10.56, 5.52);
 
-    cout<<"Add of ar1: "<<ar2.add()<<endl;
-    cout<<"Sub of ar1: "<<ar2.sub()<<endl;
+    cout << "Add of ar1: " << ar2.add() << endl;
+    cout << "Sub of ar1: " << ar2.sub() << endl;
 
     return 0;
 }
