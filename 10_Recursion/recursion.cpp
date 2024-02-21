e268ff7efcc66bcd87003f5adac0ffd0bdcfd5e7
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-02-20 20:57:49 -0500

    Recursion - Basic

---
+++ 10_Recursion/recursion.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+void fun1(int n){
+    if(n > 0){
+        cout<<"Fun 1: " << n << endl;
+        fun1(n-1);
+    }
+
+}
+
+void fun2(int n){
+    if(n > 0){
+        fun2(n-1);
+        cout << "Fun 2: " << n << endl;
+    }
+}
+
+int main(){
+
+    int x=3;
+    
+    fun1(x);
+
+    return 0;
+}
