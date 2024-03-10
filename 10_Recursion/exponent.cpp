5ded67cc31f4267684eb826773b249e21faa43b8
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-03-10 12:27:13 -0400

    Recursion - Exponent Improved

--- 10_Recursion/exponent.cpp
+++ 10_Recursion/exponent.cpp
@@ -9,9 +9,21 @@ int exponent(int m, int n){
         return 1;
 }
 
+//Efficient way to calculate exponents
+int power(int m, int n){
+    if(n == 0)
+        return 1;
+    if(n % 2 == 0)
+        return power(m * m, n/2);
+    else
+        return m * power(m * m, (n-1)/2);
+}
+
 int main(){
 
-    cout<< exponent(3, 4) << endl;
+    cout << exponent(3, 4) << endl;
+
+    cout << power(2, 9) << endl;
 
     return 0;
 }
