9d2de42d2a2ebc75854348a0e3217082abe77bff
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-03-21 18:38:13 -0400

    Recursion - Fibonacci using Memoization

--- 10_Recursion/fibonacci.cpp
+++ 10_Recursion/fibonacci.cpp
@@ -3,12 +3,14 @@
 
 using namespace std;
 
+/* Excessive Recursion */
 int fib(int n){
     if(n <= 1)
         return n;
     return fib(n-2) + fib(n-1); 
 }
 
+/* Iteration */
 int fib_loop(int n){
     int t0 = 0, t1 = 1, s;
     if(n <= 1)
@@ -23,9 +25,35 @@ int fib_loop(int n){
     return s;
 }
 
+/* Recursion using Memoization */
+int F[10];
+
+int fib_mem(int n){
+    if(n <= 1){
+        F[n] = n;
+        return n;
+    }
+    else{
+        if(F[n-2] == -1)
+            F[n-2] = fib_mem(n-2);
+        if(F[n-1] == -1)
+            F[n-1] = fib_mem(n-1);
+
+        F[n] = F[n-2] + F[n-1];
+        return F[n-2] + F[n-1];
+    }
+}
+
+
 int main(){
 
-    cout << fib(7) << endl;
+    cout << fib(6) << endl;
     cout << fib_loop(7) << endl;
+
+    for(int i=0; i < 10; i++)
+        F[i] = -1;
+    
+    cout<< fib_mem(8) << endl;
+
     return 0;
 }
