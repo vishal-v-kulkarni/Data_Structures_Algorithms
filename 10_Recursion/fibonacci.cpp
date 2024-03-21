74143f8be021cd73addaa13661ea6804af62971c
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-03-21 18:04:18 -0400

    Recursion - Fibonacci using Iteration

--- 10_Recursion/fibonacci.cpp
+++ 10_Recursion/fibonacci.cpp
@@ -9,8 +9,23 @@ int fib(int n){
     return fib(n-2) + fib(n-1); 
 }
 
+int fib_loop(int n){
+    int t0 = 0, t1 = 1, s;
+    if(n <= 1)
+        return n;
+    
+    for(int i = 2; i <= n; i++){
+        s = t0 + t1;
+        t0 = t1;
+        t1 = s;
+    }
+
+    return s;
+}
+
 int main(){
 
-    cout << fib(7) << endl; 
+    cout << fib(7) << endl;
+    cout << fib_loop(7) << endl;
     return 0;
 }
