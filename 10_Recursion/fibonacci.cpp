d4082488b17a965baad2ab809fbaef30a44e9fdf
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-03-21 17:58:55 -0400

    Recursion - Fibonacci using Recursion

---
+++ 10_Recursion/fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int fib(int n){
+    if(n <= 1)
+        return n;
+    return fib(n-2) + fib(n-1); 
+}
+
+int main(){
+
+    cout << fib(7) << endl; 
+    return 0;
+}
