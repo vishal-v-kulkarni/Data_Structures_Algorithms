e29eeec425346a5c1b58a2d0c21df5412a70bf2e
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-07-16 17:30:37 -0400

    Array ADT - Merge Arrays

--- 12_Array_ADT/array_adt.cpp
+++ 12_Array_ADT/array_adt.cpp
@@ -275,6 +275,29 @@ void NegativeArrange(struct Array *arr){
 
 }
 
+struct Array * MergeArrays(struct Array arr1, struct Array arr2){
+    int i=0, j=0, k=0;
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+    while(i < arr1.length && j < arr2.length){
+        if(arr1.A[i] <= arr2.A[j])
+            arr3->A[k++] = arr1.A[i++];
+        else
+            arr3->A[k++] = arr2.A[j++];
+    }
+
+    for(;i<arr1.length; i++)
+        arr3->A[k++] = arr1.A[i];
+    for(;j<arr2.length; j++)
+        arr3->A[k++] = arr1.A[j];
+
+    arr3->length = arr1.length + arr2.length;
+    arr3->size = 10;    
+
+    return arr3;
+
+}
+
 
 int main(){
     // Dynamic Array Creation
@@ -335,7 +358,14 @@ int main(){
 
     NegativeArrange(&arr);
 
-    Display(arr);
+    struct Array arr1 = {{3, 8, 16, 20, 25}, 10, 5};
+    struct Array arr2 = {{4, 10, 12, 22, 23}, 10, 5};
+    struct Array *arr3;
+    arr3 = MergeArrays(arr1, arr2);
+
+    Display(arr1);
+    Display(arr2);
+    Display(*arr3);
     return 0;
 
 }
