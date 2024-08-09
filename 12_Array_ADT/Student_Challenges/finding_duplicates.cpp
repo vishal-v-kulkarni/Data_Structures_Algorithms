f62d0db598b51b8383753950ace7945e4b1a5a10
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-08-08 20:04:00 -0400

    Finding Duplicates - Unsorted Array using 2 methods

--- 12_Array_ADT/Student_Challenges/finding_duplicates.cpp
+++ 12_Array_ADT/Student_Challenges/finding_duplicates.cpp
@@ -39,32 +39,65 @@ void CountingDuplicates(int A[], int n)
     }
 }
 
-// Finding and Counting Duplicated using Hashing - Method 2 - O(n)
+// Finding and Counting Duplicated using Hashing - Method 2 - O(n) - Works for sorted and unsorted
 void DuplicatesUsingHashing(int A[], int n, int l, int h)
 {
-    int *H = new int[h];
-
+    int *H = new int[h + 1];
+    //  int A[] = {8, 3, 6, 4, 6, 5, 6, 8, 2, 7};
     for (int i = 0; i < n; i++)
     {
         H[A[i]]++;
     }
 
-    for (int j = l; j < h; j++)
+    for (int j = l; j < h + 1; j++)
     {
         if (H[j] > 1)
             cout << "Element " << j << " appears " << H[j] << " times." << endl;
     }
 }
 
+// Finding and counting duplicates for unsorted array
+void DuplicatesUnsorted(int A[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int count = 1;
+        if (A[i] != -1)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (A[i] == A[j])
+                {
+                    count++;
+                    A[j] = -1;
+                }
+            }
+            if (count > 1)
+                cout << "Element " << A[i] << " appears " << count << " times." << endl;
+        }
+    }
+}
+
 int main()
 {
-    int A[] = {3, 6, 8, 8, 12, 12, 15, 15, 15, 20};
+    // Sorted Array
+    // int A[] = {3, 6, 8, 8, 12, 12, 15, 15, 15, 20};
+    // int n = 10;
+    // int l = 3, h = 20;
+
+    // Unsorted Array
+    int A[] = {8, 3, 6, 4, 6, 5, 6, 8, 2, 7};
     int n = 10;
-    int l = 3, h = 20;
+    int l = 2;
+    int h = 8;
 
     // FindingDuplicates(A, n);
     // CountingDuplicates(A, n);
     DuplicatesUsingHashing(A, n, l, h);
+
+    // DuplicatesUnsorted(A, n);
+
+    int A[h + 1][2];
 
     return 0;
 }
