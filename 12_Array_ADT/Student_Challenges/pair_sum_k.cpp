29ed6384496afc0beae3f36498deaffb2891a5ce
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-08-12 13:58:50 -0400

    Student Challenge - Pair Sum K - Sorted Array - 1 method

--- 12_Array_ADT/Student_Challenges/pair_sum_k.cpp
+++ 12_Array_ADT/Student_Challenges/pair_sum_k.cpp
@@ -37,16 +37,47 @@ void PairSumK_Hashing(int A[], int l, int h, int n, int k)
     }
 }
 
+// Sorted Array - Using 2 pointers - O(n)
+void PairSumK_TwoPointers(int A[], int n, int k)
+{
+    int i = 0, j = n - 1;
+    while (i < j)
+    {
+        if (A[i] + A[j] == k)
+        {
+            cout << "Sum of " << A[i] << " and " << A[j] << " is " << k << "." << endl;
+            i++, j--;
+        }
+        else if (A[i] + A[j] > k)
+        {
+            j--;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
+
 int main()
 {
+    int k = 10;
+
     // Unsorted Array - 2 methods
     int A[] = {6, 3, 8, 10, 16, 7, 5, 2, 4, 14};
     int l = 2, h = 16;
     int n = 10;
-    int k = 10;
 
+    // Sorted Array
+    int A1[] = {1, 3, 4, 5, 6, 8, 9, 10, 12, 14};
+    n = 10;
+
+    // Function calls for Unsorted Array
     // PairSumK_Scan(A, n, k);
-    PairSumK_Hashing(A, l, h, n, k);
+    // PairSumK_Hashing(A, l, h, n, k);
+
+    // Function Calls for Sorted Array
+    PairSumK_TwoPointers(A1, n, k);
 
     return 0;
 }
