48d1e019fc780948a4ec31249c71e0a7c9157348
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-08-26 18:54:55 -0400

    String Operations - Vowels, Consonants and Words Count functions

--- 13_Strings/string_operations.cpp
+++ 13_Strings/string_operations.cpp
@@ -27,6 +27,37 @@ void toggleCase(char S[])
     cout << S << endl;
 }
 
+void vowelsConsonantsCount(char S[])
+{
+    int i, vcount = 0, ccount = 0;
+    for (i = 0; S[i] != '\0'; i++)
+    {
+        if (S[i] == 'a' || S[i] == 'e' || S[i] == 'i' || S[i] == 'o' || S[i] == 'u' || S[i] == 'A' || S[i] == 'E' || S[i] == 'I' || S[i] == 'O' || S[i] == 'U')
+        {
+            vcount++;
+        }
+        else if ((S[i] >= 65 && S[i] <= 90) || (S[i] >= 97 && S[i] <= 122))
+        {
+            ccount++;
+        }
+    }
+    cout << "Number of Vowels in the sentence are " << vcount << endl;
+    cout << "Number of Consonants in the sentence are " << ccount << endl;
+}
+
+void wordsCount(char S[])
+{
+    int i, words = 0;
+
+    for (i = 0; S[i] != '\0'; i++)
+    {
+        if (S[i] == ' ' && S[i - 1] != ' ')
+            words++;
+    }
+
+    cout << "Number of words in the sentence are " << words + 1 << endl;
+}
+
 int main()
 {
 
@@ -36,6 +67,10 @@ int main()
     cout << "The length of the string is " << length << endl;
 
     toggleCase(S);
+
+    char A[] = "How are    you"; // Big spaces are known as 'White Spaces'
+    vowelsConsonantsCount(A);
+    wordsCount(A);
 
     return 0;
 }
