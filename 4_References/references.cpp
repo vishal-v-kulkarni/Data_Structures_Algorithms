475ec6f1224468861a6755db652cd890f7484754
Vishal Kulkarni <vkvishal1508@gmail.com>
2025-01-17 12:12:57 -0500

    References revision

--- 4_References/references.cpp
+++ 4_References/references.cpp
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+
     int a = 10;
-    int &r = a; //Reference cannot be changed once assigned.
+    int &r = a; // Reference cannot be changed once assigned.
 
     int b = 25;
-    r = b; //This is an assignment of value (25) and not changing the reference. This will still point to a.
+    r = b; // This is an assignment of value (25) and not changing the reference. This will still point to a.
 
-    cout<<r<<endl;
+    cout << r << endl;
+    cout << a << endl; // Same as the value of 'b'
 
     return 0;
 }
