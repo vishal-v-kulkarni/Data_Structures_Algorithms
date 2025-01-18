d5350f76deec8d8a1f97f96fa052139f8e61da13
Vishal Kulkarni <vkvishal1508@gmail.com>
2025-01-18 13:10:25 -0500

    Revision: Pointer to Structure

--- 4_References/pointer_to_struct.cpp
+++ 4_References/pointer_to_struct.cpp
@@ -2,41 +2,52 @@
 #include <stdlib.h>
 using namespace std;
 
-struct Rectangle{
+struct Rectangle
+{
     int length;
     int breadth;
 };
 
-int main(){
+int main()
+{
 
-    //Pointer to a struct in a Stack.
-    struct Rectangle r = {10,5};
+    // Pointer to a struct in a Stack.
+    struct Rectangle r = {10, 5};
     struct Rectangle *p = &r;
-    
-    //p=&r;
+
+    // p=&r;
 
     r.length = 15;
     (*p).length = 20; // . takes precedence over * So add brackets
-    p->length = 25; //Another way to access the member
+    p->length = 25;   // Another way to access the member
 
-    cout<< "Pointer to a struct in the STACK: " <<endl;
-    cout<<p<<endl;
-    cout<<"Length of Rectangle is "<< p->length<<endl;
-    cout<<"Breadth of Rectangle is "<< p->breadth<<endl<<endl;
-    
-    //Pointer to a struct in HEAP
+    cout << "Pointer to a struct in the STACK: " << endl;
+    cout << p << endl;
+    cout << "Length of Rectangle is " << p->length << endl;
+    cout << "Breadth of Rectangle is " << p->breadth << endl
+         << endl;
+
+    // Pointer to a struct in HEAP
     Rectangle *p1;
     p1 = (Rectangle *)malloc(sizeof(Rectangle)); // Without struct keyword is C++ syntax.
-    //p1 = new Rectangle; //C++ Syntax
+    // p1 = new Rectangle; //C++ Syntax
     p1->length = 10;
     p1->breadth = 5;
 
-    cout<< "Pointer to a struct in the HEAP: " <<endl;
-    cout<<p1<<endl;
-    cout<<"Length of Rectangle is "<< p1->length<<endl;
-    cout<<"Breadth of Rectangle is "<< p1->breadth<<endl;
-    
+    cout << "Pointer to a struct in the HEAP: " << endl;
+    cout << p1 << endl;
+    cout << "Length of Rectangle is " << p1->length << endl;
+    cout << "Breadth of Rectangle is " << p1->breadth << endl;
+
     delete p1;
+
+    // Revision
+    struct Rectangle *p3 = (Rectangle *)malloc(sizeof(Rectangle));
+
+    p3->breadth = 5;
+    p3->length = 10;
+
+    cout << p3->length << endl;
 
     return 0;
 }
