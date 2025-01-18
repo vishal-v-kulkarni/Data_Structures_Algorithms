895e1a25f806dd2847c26eaa5cef4fbab9215473
Vishal Kulkarni <vkvishal1508@gmail.com>
2025-01-18 13:14:23 -0500

    Revision: Functions

--- 6_Functions/functions.cpp
+++ 6_Functions/functions.cpp
@@ -1,20 +1,22 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
 
-int add(int a, int b){ //Prototype/Signature/Declaration of a function / Formal parameters
+int add(int a, int b)
+{ // Prototype/Signature/Declaration of a function / Formal parameters
     int c;
-    c = a+b;
-    return c; //The body with {} is called definition of the function
+    c = a + b;
+    return c; // The body with {} is called definition of the function
 }
 
-int main(){
+int main()
+{
     int x, y, z;
     x = 10;
-    y=5;
+    y = 5;
 
-    z = add(x, y); //Function call - Actual parameters
-    cout<<"Sum of x and y is "<< z <<endl;
+    z = add(x, y); // Function call - Actual parameters
+    cout << "Sum of x and y is " << z << endl;
 
     return 0;
 }
