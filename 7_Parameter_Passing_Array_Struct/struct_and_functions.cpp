089702eaf855d22e0650c8c78c736df472ce8720
Vishal Kulkarni <vkvishal1508@gmail.com>
2025-01-22 11:07:20 -0500

    Revision: Struct and Functions

--- 7_Parameter_Passing_Array_Struct/struct_and_functions.cpp
+++ 7_Parameter_Passing_Array_Struct/struct_and_functions.cpp
@@ -1,34 +1,39 @@
-#include<iostream>
-#include<stdio.h>
-#include<stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
-struct Rectangle{
+struct Rectangle
+{
     int length;
     int breadth;
 };
 
-void initialize(struct Rectangle *r, int l, int b){
+void initialize(struct Rectangle *r, int l, int b)
+{
     r->length = l;
     r->breadth = b;
 }
 
-int area(struct Rectangle r){
-    return r.length*r.breadth;
+int area(struct Rectangle r)
+{
+    return r.length * r.breadth;
 }
 
-void changeLength(struct Rectangle *r, int l){
+void changeLength(struct Rectangle *r, int l)
+{
     r->length = l;
 }
 
-
-int main(){
+int main()
+{
 
     struct Rectangle r;
 
+    // Revision
     initialize(&r, 10, 20);
     int ar = area(r);
-    changeLength(&r,15);
+    changeLength(&r, 15);
 
     return 0;
 }
