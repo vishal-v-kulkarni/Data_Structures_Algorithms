2f46545446438b3448856d0aab8ebb7a2ee6db95
Vishal Kulkarni <vkvishal1508@gmail.com>
2025-02-03 08:50:16 -0500

    Revision: Recursion Static Variable

--- 10_Recursion/recursion_static_var.cpp
+++ 10_Recursion/recursion_static_var.cpp
@@ -1,35 +1,41 @@
-#include<stdio.h>
-#include<iostream>
+#include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
-int fun2(int n){
+int fun2(int n)
+{
 
-    if(n > 0){
-        return fun(n-1) + n;
+    if (n > 0)
+    {
+        return fun(n - 1) + n;
     }
     return 0;
 }
 
-//int x=0; //Global variable is also the same.
+// int x=0; //Global variable is also the same.
+// Revision: Final x value will be there in all addition operation on stack - Check Stack Tree
 
-int fun(int n){
+int fun(int n)
+{
 
-    static int x=0;
+    static int x = 0;
 
-    if(n > 0){
+    if (n > 0)
+    {
         x++;
-        return fun(n-1) + x;
+        return fun(n - 1) + x;
     }
     return 0;
 }
 
-int main(){
-    
+int main()
+{
+
     int result;
     result = fun(5);
 
-    cout<<result<<endl;
-    
+    cout << result << endl;
+
     return 0;
 }
