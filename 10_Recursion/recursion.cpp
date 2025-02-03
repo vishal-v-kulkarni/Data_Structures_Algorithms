ed6e04a09ed252b72ff23e2c244a25a0b6342040
Vishal Kulkarni <vkvishal1508@gmail.com>
2025-02-03 08:44:44 -0500

    Revision: Head and Tail Recursion

--- 10_Recursion/recursion.cpp
+++ 10_Recursion/recursion.cpp
@@ -1,30 +1,36 @@
-#include<stdio.h>
-#include<iostream>
+#include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
 /* Tail Recursion */
-void fun1(int n){
-    if(n > 0){
-        cout<<"Fun 1: " << n << endl;
-        fun1(n-1);
+void fun1(int n)
+{
+    if (n > 0)
+    {
+        cout << "Fun 1: " << n << endl;
+        fun1(n - 1);
     }
-
 }
 
 /* Head Recursion */
-void fun2(int n){
-    if(n > 0){
-        fun2(n-1);
+void fun2(int n)
+{
+    if (n > 0)
+    {
+        fun2(n - 1);
         cout << "Fun 2: " << n << endl;
     }
 }
 
-int main(){
+int main()
+{
 
-    int x=3;
-    
+    int x = 3;
+
     fun1(x);
 
     return 0;
 }
+
+// Revision
