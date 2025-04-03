166e0cc21a8ae7885f816cf54483801c2f6e67a9
Vishal Kulkarni <vkvishal1508@gmail.com>
2025-04-03 16:36:23 -0400

    Linked List - Circular - Creation and Display

--- 17_Linked_List/LL_circular.cpp
+++ 17_Linked_List/LL_circular.cpp
@@ -14,7 +14,7 @@ void create(int A[], int n)
 
     head = new Node;
     head->data = A[0];
-    head->next = head;
+    head->next = head; // Circular
 
     last = head; // Last will be initially pointing to first
 
@@ -22,7 +22,7 @@ void create(int A[], int n)
     {
         t = new Node;
         t->data = A[i];
-        t->next = NULL;
+        t->next = head; // Circular
 
         // Connecting the previous node to the next node and moving the last pointer.
         last->next = t;
@@ -30,7 +30,40 @@ void create(int A[], int n)
     }
 }
 
+// Display using do-while loop
+void display(Node *p)
+{
+
+    do
+    {
+        cout << p->data << " ";
+        p = p->next;
+
+    } while (p != head);
+}
+
+// Display using flag and recursion
+void displayRec(Node *p)
+{
+    static bool flag = false; // Static is important, so that it is changing only once.
+    if (p != head || flag == false)
+    {
+        flag = true;
+        cout << p->data << " ";
+        displayRec(p->next);
+    }
+    flag = false;
+}
+
 int main()
 {
+    int A[5] = {2, 5, 9, 1, 8};
+    create(A, 5);
+
+    display(head);
+    cout << endl;
+
+    displayRec(head);
+
     return 0;
 }
