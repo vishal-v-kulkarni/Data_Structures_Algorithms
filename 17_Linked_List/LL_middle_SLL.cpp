650f87f869df85d29c47ada60e0525c696139a2c
Vishal Kulkarni <vkvishal1508@gmail.com>
2025-04-07 15:55:28 -0400

    Singly Linked List - Middle Node

---
+++ 17_Linked_List/LL_middle_SLL.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL, *last = NULL;
+
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+
+    last = first; // Last will be initially pointing to first
+
+    for (int i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+
+        // Connecting the previous node to the next node and moving the last pointer.
+        last->next = t;
+        last = t;
+    }
+}
+
+// Count loop
+int count(struct Node *p)
+{
+    int count = 0;
+    while (p != NULL)
+    {
+        count++;
+        p = p->next;
+    }
+
+    return count;
+}
+
+int findMiddle(Node *p)
+{
+    Node *q = first;
+
+    while (q)
+    {
+        q = q->next;
+
+        if (q)
+            q = q->next;
+        if (q)
+            p = p->next;
+    }
+    return p->data;
+}
+
+int main()
+{
+    int A[8] = {8, 6, 3, 9, 10, 5, 4, 12};
+    create(A, 8);
+
+    cout << "The middle node of the LL is: " << findMiddle(first) << endl;
+
+    return 0;
+}
